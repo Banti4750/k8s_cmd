638c220823b30cb13692ab8bb219678075ca4d26
Banti4750 <bantikumar6203818460@gmail.com>
2026-04-01 18:16:14 +0530

    deployment in k8s

---
+++ deployment/app.sh
@@ -0,0 +1,13 @@
+kubectl apply -f deployment.yml
+kubectl get deployments/nginx-deployment
+kubectl get deployments/nginx-deployment -o wide
+kubectl get deployments -o yaml
+kubectl get deployments -o json
+kubectl get deployments -o name
+kubectl delete deployment nginx-deployment
+kubectl delete deployment nginx-deployment --grace-period=0 --force
+
+kubectl logs deployment/nginx-deployment
+kubectl describe deployment nginx-deployment
+kubectl rollout status deployment/nginx-deployment
+kubectl scale deployment/nginx-deployment --replicas=3
