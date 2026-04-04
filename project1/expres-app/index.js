fd1dc854e5ccf4c80653a8cc7ab9eb9de284878a
Banti4750 <bantikumar6203818460@gmail.com>
2026-04-04 14:05:34 +0530

    added redis

--- project1/expres-app/index.js
+++ project1/expres-app/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { Pool } from 'pg';
+import ioredis from 'ioredis';
 
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+
+const redis = new ioredis({
+    host: 'redis.default.svc.cluster.local',
+    port: 6379,
+});
 
 const pool = new Pool({
   connectionString: "postgres://postgres:postgres@db.default.svc.cluster.local:5432/postgres",
@@ -14,6 +20,7 @@ const pool = new Pool({
 app.get('/api/data', async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM users");
+    redis.set('users', JSON.stringify(result.rows), 'EX', 600); // Cache for 1 hour
     res.json(result.rows);
   }
     catch (err) {
@@ -21,6 +28,23 @@ app.get('/api/data', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.get('/api/data/cached', async (req, res) => {
+    try {
+        const cachedData = await redis.get('users');
+        if (cachedData) {
+            return res.json(JSON.parse(cachedData));
+        }
+        const result = await pool.query("SELECT * FROM users");
+        redis.set('users', JSON.stringify(result.rows), 'EX', 600);
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.post('/api/data', async (req, res) => {
   const { name, email } = req.body;
